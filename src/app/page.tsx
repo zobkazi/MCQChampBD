deddb28921ff7fde04c2a20e5b669593973c1568
zobkazi <zobaidulkazi64.official@gmail.com>
2024-05-03 23:08:36 +0600

    ads as

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className="text-3xl font-bold underline m-0 after:text-3xl">Welcome to Job Board</h1>
+    </div>
   )
 }
 
