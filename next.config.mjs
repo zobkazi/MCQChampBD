9e133498494dfec7edfaea3c7db106b12e7ed783
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-12-30 14:34:52 +0600

    next-config-v:0.0.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+/* @ts-check */
+/** @see https://nextjs.org/docs/api-reference/next.config.js/introduction */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    headers: require('./headers.config'),
+    webpack: require('./webpack.config'),
+    reactStrictMode: true,
+    swcMinify: true,
+    pageExtensions: ['ts', 'tsx']
+  };
+  
+  module.exports = nextConfig;
+  
