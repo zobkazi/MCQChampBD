f64bcb27d65befe1df0532580808d15af1aebc67
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-01 15:44:24 +0600

    multiple root layouts-3

---
+++ src/layout/HomeLayout.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "@/styles/globals.css";
+import TopNavbar from "@/components/pages/Home/TopNavbar";
+
+
+export default async function HomeLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (   
+            <div>
+              <TopNavbar />
+              <main className="flex-grow container mx-auto px-4 pt-20">
+                {children}
+              </main>
+
+              </div>
+          
+    );
+  }
+  
