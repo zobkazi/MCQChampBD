c8846ddb45050612640ada1694a00086f483963a
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-01-01 15:40:55 +0600

    multiple root layouts-1

---
+++ src/app/(admin)/login/page.tsx
@@ -0,0 +1,13 @@
+
+import GetDivisions from '@/components/forms/registration/getDivisions'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <GetDivisions />
+    </div>
+  )
+}
+
+export default Login
---
+++ src/app/(admin)/signup/page.tsx
@@ -0,0 +1,12 @@
+import RegistrationForm from '@/components/forms/registration/RegistrationForm'
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+        <RegistrationForm />
+    </div>
+  )
+}
+
+export default SignUp
